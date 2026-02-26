40903281d243cb51325ea9f43be0dc5240ce9830
Esmerlin <severinoesmerlin1624@gmail.com>
2026-02-26 11:17:44 -0400

    Mejoras en la lógica del árbol de comportamiento y ajustes en la clase Enemigo. Se implementan nuevas funcionalidades para la detección de colisiones y el manejo de estados del jugador.

--- scripts/player.py
+++ scripts/player.py
@@ -8,55 +8,64 @@ class Player:
 
         self.Mapa = Mapa  # Guardamos el mapa dentro del jugador para poder revisar colisiones.
 
-        self.Fila = 1  # Posición inicial vertical en el grid 
-        self.Columna = 1  # Posición inicial horizontal en el grid 
+        self.Fila = 1.0  # Posición inicial vertical en la cuadricula 
+        self.Columna = 1.0  # Posición inicial horizontal en el cuadricula
 
         self.Color = (0, 255, 0)  # Color De mi jugador 
 
-        self.Velocidad = 2  # Velocidad en píxeles por segundo, que va a tener el jugador. 
+        self.Velocidad = 4.0  # Velocidad en píxeles por segundo, que va a tener el jugador. 
 
        # Función que mueve el jugador
     
     def Mover(self, Teclas, DeltaTiempo):   
 
-        NuevaFila = self.Fila  # Variable temporal que voy a utilizar para probar movimiento vertical
-        NuevaColumna = self.Columna  
+        NuevaFila = self.Fila # Para calcular la nueva funsion vertical del jugador 
+        NuevaColumna = self.Columna # Para calcular la nueva funsion horizontal del jugador
 
-        if Teclas[pygame.K_UP]:  # Si la flecha arriba está presionada 
-            NuevaFila -= 1  # Restamos una fila (sube en el grid)
+        if Teclas[pygame.K_UP]:  # si se presiona la flecha hacia arriba, se mueve hacia arriba 
+            NuevaFila -= self.Velocidad * DeltaTiempo 
+        if Teclas[pygame.K_DOWN]:  # si se presiona la flecha hacia abajo, se mueve hacia abajo
+            NuevaFila += self.Velocidad * DeltaTiempo
+        if Teclas[pygame.K_LEFT]:  #si se presiona la flecha hacia la izquierda, se mueve hacia la izquierda
+            NuevaColumna -= self.Velocidad * DeltaTiempo
+        if Teclas[pygame.K_RIGHT]:  # si se presiona la flecha hacia la derecha, se mueve hacia la derecha
+            NuevaColumna += self.Velocidad * DeltaTiempo
 
-        if Teclas[pygame.K_DOWN]:  # Si la flecha abajo está presionada
-            NuevaFila += 1 
+       
+        # Añadimos un margen (0.2) para que el "cuadrado" del jugador no entre en la pared
+        margen = 0.3
 
-        if Teclas[pygame.K_LEFT]:  # Si la flecha izquierda está presionada
-            NuevaColumna -= 1  
+        # Revisar colisión en Columna (Eje X)
+        # Evaluamos el lado izquierdo y derecho del jugador
+        if self.Mapa.Cuadricula[int(self.Fila + margen)][int(NuevaColumna + margen)] != 1 and \
+           self.Mapa.Cuadricula[int(self.Fila + 1 - margen)][int(NuevaColumna + margen)] != 1 and \
+           self.Mapa.Cuadricula[int(self.Fila + margen)][int(NuevaColumna + 1 - margen)] != 1 and \
+           self.Mapa.Cuadricula[int(self.Fila + 1 - margen)][int(NuevaColumna + 1 - margen)] != 1:
+            self.Columna = NuevaColumna
 
-        if Teclas[pygame.K_RIGHT]:  # Si la flecha derecha está presionada
-            NuevaColumna += 1  
-
-        # Verifico que la nueva posición esté dentro de los límites del mapa y que no sea una pared
-        if 0 <= NuevaFila < len(self.Mapa.Cuadricula) and \
-           0 <= NuevaColumna < len(self.Mapa.Cuadricula[0]) and \
-          self.Mapa.Cuadricula[NuevaFila][NuevaColumna] != 1:
-
-          self.Fila = NuevaFila
-          self.Columna = NuevaColumna
+        # Revisar colisión en Fila (Eje Y)
+        # Evaluamos el lado superior e inferior del jugador
+        if self.Mapa.Cuadricula[int(NuevaFila + margen)][int(self.Columna + margen)] != 1 and \
+           self.Mapa.Cuadricula[int(NuevaFila + 1 - margen)][int(self.Columna + margen)] != 1 and \
+           self.Mapa.Cuadricula[int(NuevaFila + margen)][int(self.Columna + 1 - margen)] != 1 and \
+           self.Mapa.Cuadricula[int(NuevaFila + 1 - margen)][int(self.Columna + 1 - margen)] != 1:
+            self.Fila = NuevaFila
          
       # Función para dibujar el jugador 
     def Dibujar(self, Pantalla): 
 
-        AnchoPantalla = Pantalla.get_width()  # Para Obtener el ancho real de la pantalla
-        AltoPantalla = Pantalla.get_height()  # Para Obtener el alto real de la pantalla
+        AnchoPantalla = Pantalla.get_width()  # Para devolver cuantos pixeles mide el ancho
+        AltoPantalla = Pantalla.get_height()  # Para devolver cuantos pixeles mide el alto
 
-        Filas = len(self.Mapa.Cuadricula)  # Cuenta cuántas filas tiene el mapa y en la siguiente linea las columnas.
-        Columnas = len(self.Mapa.Cuadricula[0]) 
+        Filas = len(self.Mapa.Cuadricula)   # Para devolver cuantas filas tiene el mapa 
+        Columnas = len(self.Mapa.Cuadricula[0]) # Para devolver cuantas columnas tiene el mapa 
 
-        TamañoCeldaX = AnchoPantalla // Columnas  # Para Calcular el ancho de cada celda
-        TamañoCeldaY = AltoPantalla // Filas  # El Alto de la celda
+        TamañoCeldaX = AnchoPantalla // Columnas  # Para dividir el ancho de la pantalla entre el numero de la columna del mapa
+        TamañoCeldaY = AltoPantalla // Filas  # Para dividir el alto de la pantalla entre el numero de fila del mapa
 
-        PosicionX = self.Columna * TamañoCeldaX  
-        PosicionY = self.Fila * TamañoCeldaY  
+        PosicionX = int(self.Columna * TamañoCeldaX)  # Para calcular la posicion horizontal del jugador multiplicando su columna por el tamaño de cada celda 
+        PosicionY = int(self.Fila * TamañoCeldaY)  # Para calcular la posicion vertical del jugador multiplicando su fila por el tamaño de cada celda 
 
-        Rectangulo = pygame.Rect(PosicionX, PosicionY, TamañoCeldaX, TamañoCeldaY)  # Crea el rectángulo del jugador
+        Rectangulo = pygame.Rect(PosicionX, PosicionY, TamañoCeldaX, TamañoCeldaY)  # para crear un rectangulocon la posicion y el tamaño el jugador
 
-        pygame.draw.rect(Pantalla, self.Color, Rectangulo)  # Dibuja el jugador 
+        pygame.draw.rect(Pantalla, self.Color, Rectangulo) # para dibujar el rectangulo del jugador en la pantalla con su color 
